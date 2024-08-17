2cc1ad8120371c5ae413482b5b728dc6b1105170
Iavor Diatchki <iavor.diatchki@gmail.com>
2024-08-17 14:58:04 -0700

    Add city tiles

--- src/Constants.hs
+++ src/Constants.hs
@@ -1,5 +1,6 @@
 module Constants where
 
+import Data.List(delete)
 import Data.Map(Map)
 import Data.Map qualified as Map
 import Types
@@ -34,4 +35,37 @@ cityCosts = Map.fromList
   , (Cloth, (5, [Brick,Cloth]))
   , (Salt,  (5, [Tool,Wine]))
   ]
+
+
+cityTiles :: Map CityTile [Resource]
+cityTiles = Map.fromList
+  [ (A, [Brick, Brick, Wheat, Wheat, Tool, Wine, Cloth])
+  , (B, [Brick, Brick, Wheat, Wheat, Tool, Tool, Wine, Cloth])
+  , (C, [Brick, Brick, Brick, Wheat, Wheat, Tool, Tool, Wine, Wine, Cloth])
+  , (D, [Brick, Wheat, Tool, Wine, Cloth])
+  ]
+
+
+cityTilesWithSalt :: Map CityTile [Resource]
+cityTilesWithSalt =
+  replace A Wheat .
+  replace B Tool  .
+  replace C Wine  .
+  replace D Brick $
+  cityTiles
+  where
+  replace x a = Map.adjust ((Salt :) . delete a) x
+
+marketCosts :: [[ResourceCost]]
+marketCosts = [ [], [Any], [Any], [cloth], [cloth], [Any,cloth], [cloth,cloth] ]
+  where
+  cloth = Resource Cloth
+
+altMarketCosts :: [[ResourceCost]]
+altMarketCosts = [ [], [Any], [Any], [wine], [cloth], [Any,wine], [wine,cloth] ]
+  where
+  cloth = Resource Cloth
+  wine  = Resource Wine
+
+
 
