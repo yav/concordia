e34661795c3e2d36558a28e7198c93e4e47563ed
Iavor Diatchki <iavor.diatchki@gmail.com>
2024-09-02 11:09:58 -0700

    Make a map flag

--- src/Main.hs
+++ src/Main.hs
@@ -4,6 +4,7 @@ import Data.Aeson qualified as JS
 import KOI.Basics
 import KOI.CallJS(jsHandlers)
 import KOI.Bag
+import KOI.Options
 import KOI.RNGM
 import KOI
 import Setup
@@ -11,45 +12,45 @@ import Play
 import Constants
 import Types
 import Cards
-import Static
+
 
 main :: IO ()
-main =
-  -- XXX
-  JS.eitherDecodeFileStrict' "ui/maps/Italia.json" >>=
-  \case
-    Left err -> fail err
-    Right mp ->
-      startApp App
-      { appId = Concordia
-      , appOptions = []
-      , appColors = [ "yellow", "red", "green", "blue", "black" ]
-      , appJS = $(jsHandlers [])
-      , appInitialState = \rng opts ps ->
-        withRNG_ rng
-          do s <- setupGame (getConfig mp opts ps)
-             pure (Right s)
-      , appStart = play
-      }
-
-
-
-getConfig :: MapLayout -> config -> [PlayerId] -> Config
-getConfig mp _opts ps = Config
-  { cfgPlayerOrder        = ps
-  , cfgMap                = mp
-  , cfgCityTiles          = cityTiles
-  , cfgResourceLimit      = 12
-  , cfgStartHireWorkers   = bagFromNumList [(Person,2), (Ship,2)]
-  , cfgStartBoardWorkers  = bagFromList [Person,Ship]
-  , cfgStartResources     = bagFromList [Brick,Wheat,Wheat,Tool,Wine,Cloth]
-  , cfgStartMoney         = 5
-  , cfgStartHouses        = 15
-  , cfgMarket             = marketCosts
-  , cfgPlayerCards        = startDeckVenus
-  , cfgMarketCards        = marketDeck False
+main = startApp App
+  { appId = Concordia
+  , appOptions = [ optMap ]
+  , appColors = [ "yellow", "red", "green", "blue", "black" ]
+  , appJS = $(jsHandlers [])
+  , appInitialState = \rng opts ps ->
+    do cfg <- getConfig opts ps
+       pure (withRNG_ rng (setupGame cfg))
+  , appStart = play
   }
 
 
 
+getConfig :: Options -> [PlayerId] -> IO Config
+getConfig opts ps = 
+  do mp <- do mb <- JS.eitherDecodeFileStrict' (getMap opts)
+              case mb of
+                Left err -> fail err
+                Right a  -> pure a
+     pure Config
+       { cfgPlayerOrder        = ps
+       , cfgMap                = mp
+       , cfgCityTiles          = cityTiles
+       , cfgResourceLimit      = 12
+       , cfgStartHireWorkers   = bagFromNumList [(Person,2), (Ship,2)]
+       , cfgStartBoardWorkers  = bagFromList [Person,Ship]
+       , cfgStartResources     = bagFromList [Brick,Wheat,Wheat,Tool,Wine,Cloth]
+       , cfgStartMoney         = 5
+       , cfgStartHouses        = 15
+       , cfgMarket             = marketCosts
+       , cfgPlayerCards        = startDeckVenus
+       , cfgMarketCards        = marketDeck False
+       }
 
+
+
+optMap :: Option
+getMap :: Options -> String
+(optMap, getMap) = optionString "map" "PATH" Nothing "Path to map.json"
